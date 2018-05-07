f518765b47a87d3f33823a1616e40ad12e306bf8
node97 <37041878+node97@users.noreply.github.com>
2018-05-07 22:13:25 +0200

    Add files via upload

--- addLivre.php
+++ addLivre.php
@@ -1,33 +1,19 @@
 <?php
-include "ajoutlivre.php";
-$existe="SELECT * FROM livre WHERE (reference='".$_POST['ref']."')";
-$que=$bd->query($existe);
-$result=$que->fetchAll(PDO::FETCH_ASSOC);
-if (count ($result)>0)
-{echo '<script>alert("Reference deja dans la base");</script>';
-}
-else{
-$fich= $_FILES['photo']['name'];
-copy($_FILES['photo']['tmp_name'],'docs/'.$_FILES['photo']['name']);
-$req="INSERT INTO livre (reference,titre,nbreLivreDispo,photo,synopsis) VALUES(:reference,:titre ,:nbre ,:photo,:synopsis)";
+require "ConnexionBD.php";
+//require "ajouterLivre.php";
+
+$bd=ConnexionBD::getInstance();
+
+// On ajoute une entrée dans la table jeux_video
+$req="INSERT INTO livre ( titre, categorie, nbreLivreDispo) VALUES( :titre , :cat , :nbre)";
+
 $res=$bd->prepare($req);
-$res->execute(array('reference'=>$_POST ["ref"] ,'titre'=>$_POST ["titre"] ,'nbre'=>$_POST["nbr"] ,'photo'=>$fich,'synopsis'=>$_POST["synopsis"]) );
+$res->execute(array('titre'=>$_POST ["titre"] ,'cat'=>$_POST["Cat"] ,'nbre'=>$_POST["nbreDeCopies"]) );
+//$res->execute($_POST) ;
+//var_dump($_POST) ;
+	if(!$res){
+		echo "error";
+	}
 
-$req2=("Select * from categorie");
-$res2=$bd->query($req2);
-$details = $res2->fetchAll(PDO::FETCH_OBJ);
-$ref=$_POST['ref'];
-foreach($details as $detail)
-{   foreach ($_POST as $ind=>$po) {
-
-    if ($detail->nom == $ind)
-    {
-        $req3=("INSERT INTO appartient_cat VALUES (:ref, :nom)");
-        $res=$bd->prepare($req3);
-        $res->execute(array("ref"=>$ref,"nom"=>$detail->nom));
-    }
-
-}
-}
-}
+echo 'Le livre a bien été ajouté !';
 ?>
---
+++ getLivre.php
@@ -0,0 +1,21 @@
+	<?php
+
+require "ConnexionBD.php";
+//include "selectionLivre.php" ;
+
+$bd=ConnexionBD::getInstance();
+$req="SELECT * FROM livre WHERE reference=:ref " ;
+$res=$bd->prepare($req);
+
+$res->execute(array('ref'=>$_GET["reference"])) ; 
+$afficheRes=$res->fetch(PDO::FETCH_ASSOC) ;	
+$myRefValue=$_GET["reference"] ;
+
+
+$maRequette="SELECT * FROM appartient_cat WHERE ref_livre=:ref " ;
+$chercherCategorie=$bd->prepare($maRequette) ;	
+
+$chercherCategorie->execute(array('ref'=>$afficheRes["reference"])) ;
+$categorieHamdi=$chercherCategorie->fetch(PDO::FETCH_ASSOC) ;
+
+	?>	
