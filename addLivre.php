62e1e30c3dbe75959010a18044f2bc38fa5b4f54
MazenHamdi <37674531+MazenHamdi@users.noreply.github.com>
2018-05-12 20:41:35 +0200

    Update addLivre.php

--- addLivre.php
+++ addLivre.php
@@ -1,18 +1,16 @@
 <?php
 require "ConnexionBD.php";
-//require "ajouterLivre.php";
 
 $bd=ConnexionBD::getInstance();
 
-// On ajoute une entrée dans la table jeux_video
+//Requéte d'ajout d'un livre
 $req="INSERT INTO livre ( titre, categorie, nbreLivreDispo) VALUES( :titre , :cat , :nbre)";
 
 $res=$bd->prepare($req);
 $res->execute(array('titre'=>$_POST ["titre"] ,'cat'=>$_POST["Cat"] ,'nbre'=>$_POST["nbreDeCopies"]) );
-//$res->execute($_POST) ;
-//var_dump($_POST) ;
+
 	if(!$res){
-		echo "error";
+		echo "Livre non ajouté, vérifier les champs";
 	}
 
 echo 'Le livre a bien été ajouté !';
